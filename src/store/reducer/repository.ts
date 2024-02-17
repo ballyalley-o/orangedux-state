431e5b76ac1cc3143d87c34c3c08b10249a9f9ba
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-02-17 16:47:15 +1300

    Refactor repository reducer

--- src/store/reducer/repository.ts
+++ src/store/reducer/repository.ts
@@ -1,19 +1,22 @@
-import {
-  IRepositoryState,
-  IRepositoryAction,
-} from '../../interface/repository-reducer'
+import { IRepositoryState } from '../../interface/repository-reducer'
 import { ActionType } from '../../typings/action'
 import { Action } from '../../constant/enum'
 
+const initialState: IRepositoryState = {
+  loading: false,
+  error: null,
+  data: [],
+}
+
 const reducer = (
-  state: IRepositoryState,
+  state: IRepositoryState = initialState,
   action: ActionType
 ): IRepositoryState => {
   switch (action.type) {
     case Action.SEARCH_REPOSITORY:
       return { loading: true, error: null, data: [] }
     case Action.SEARCH_REPOSITORY_SUCCESS:
-      return { loading: false, error: null, data: action.payload as string[] }
+      return { loading: false, error: null, data: action.payload }
     case Action.SEARCH_REPOSITORY_ERROR:
       return { loading: false, error: action.payload, data: [] }
     default:
