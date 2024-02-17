583446bcd6979409eb2847acc5300e66ba59253f
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-02-17 16:45:41 +1300

    Add Redux store and RepoList component to App.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Provider } from 'react-redux'
+import { store } from './store'
+import { RepoList } from './component'
+// @global
+import { GLOBAL } from './config'
 
 const App = () => {
+  useEffect(() => {
+    document.title = GLOBAL.APP_TITLE
+  })
 
   return (
-    <div>app</div>
+    <Provider store={store}>
+      <div>
+        <h1> Search for a NPM Package</h1>
+        <RepoList />
+      </div>
+    </Provider>
   )
 }
 
