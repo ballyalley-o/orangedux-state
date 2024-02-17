d0e7867f9e4b612f7e038577e7842416223f133f
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-02-17 16:46:05 +1300

    Add ReactDOM.createRoot for rendering App component

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,7 @@
-import { useEffect } from "react";
+import ReactDOM from 'react-dom/client'
+import App from './App'
+
+const el = document.getElementById('root')
+const root = ReactDOM.createRoot(el!)
+
+root.render(<App />)
